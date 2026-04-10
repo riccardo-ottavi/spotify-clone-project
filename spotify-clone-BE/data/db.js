9d480920fc2cc56dd62e6036f624b9c87ae5144a
riccardo-ottavi <ottavi.ps@libero.it>
2026-04-10 14:35:43 +0200

    rimetti connessione db

--- spotify-clone-BE/data/db.js
+++ spotify-clone-BE/data/db.js
@@ -1,16 +1,16 @@
 
-// const mysql = require('mysql2');
+const mysql = require('mysql2');
 
-// const db = mysql.createPool({
-// host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0
-// }).promise();
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+}).promise();
 
 
 
-// module.exports = db;
+module.exports = db;
--- spotify-clone-FE/vite.config.ts
+++ spotify-clone-FE/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // così i path sono relativi e funziona anche da filesystem o Docker
+  base: '/', 
 });
