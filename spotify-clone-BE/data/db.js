eb8cabc78e5d39b5420c09148f91836bce66f121
riccardo-ottavi <ottavi.ps@libero.it>
2026-04-10 16:43:36 +0200

    fix file  db

--- spotify-clone-BE/data/db.js
+++ spotify-clone-BE/data/db.js
@@ -1,5 +1,11 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool(process.env.DATABASE_URL).promise();
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+}).promise();
 
 module.exports = db;
