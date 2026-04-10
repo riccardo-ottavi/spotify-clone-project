30b58c1f37d05fcaf22738a40cf2c406eac133b5
riccardo-ottavi <ottavi.ps@libero.it>
2026-04-10 16:10:40 +0200

    fix alle env connessione db

--- spotify-clone-BE/data/db.js
+++ spotify-clone-BE/data/db.js
@@ -1,16 +1,5 @@
+const mysql = require("mysql2");
 
-const mysql = require('mysql2');
-
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-}).promise();
-
-
+const db = mysql.createPool(process.env.DATABASE_URL).promise();
 
 module.exports = db;
