7cfb92d0e92e3a80761297fabb45e37747a4d0ba
riccardo-ottavi <ottavi.ps@libero.it>
2026-04-13 16:35:29 +0200

    usa memoryStorage

--- spotify-clone-BE/controllers/uploadController.js
+++ spotify-clone-BE/controllers/uploadController.js
@@ -1,17 +1,7 @@
 const multer = require("multer");
-const path = require("path");
 const cloudinary = require("../config/cloudinary");
-const fs = require("fs");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "/tmp"); // Render safe
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + path.extname(file.originalname));
-  },
-});
+const storage = multer.memoryStorage(); 
 
 const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image/")) {
@@ -23,17 +13,27 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({ storage, fileFilter });
 
+const streamifier = require("streamifier");
+
 const uploadImage = async (req, res) => {
   try {
+    console.log("FILE:", req.file);
+
     if (!req.file) {
       return res.status(400).json({ error: "File non ricevuto da multer" });
     }
 
-    const result = await cloudinary.uploader.upload(req.file.path, {
-      folder: "playlists",
-    });
+    const result = await new Promise((resolve, reject) => {
+      const stream = cloudinary.uploader.upload_stream(
+        { folder: "playlists" },
+        (error, result) => {
+          if (result) resolve(result);
+          else reject(error);
+        }
+      );
 
-    fs.unlinkSync(req.file.path);
+      streamifier.createReadStream(req.file.buffer).pipe(stream);
+    });
 
     return res.status(200).json({
       url: result.secure_url,
